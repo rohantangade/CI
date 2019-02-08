966a9c864247405a7dfa7f50b1549c2267490de3
rohan <rohantangade@hotmail.com>
2019-02-08 22:40:42 +0530

    Added Unit test project

---
+++ CI/ArithmeticOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CI
+{
+    public static class ArithmeticOperation
+    {
+        public static double Addition(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+using CI;
+namespace UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange Data
+            var number1 = 10;
+            var number2 = 15;
+            var expectedValue = 25;
+
+            //Acts
+            var sum = ArithmeticOperation.Addition(number1, number2);
+
+            //Assert
+            Assert.Equal(expectedValue, sum, 1);
+        }
+    }
+}
